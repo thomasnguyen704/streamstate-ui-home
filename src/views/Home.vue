f9c191c1d535366953465e9cacb00968d4a4ff2f
thomasnguyen704 <thomasnguyen704@gmail.com>
2021-03-31 18:30:22 -0400

    clean up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,22 +5,24 @@
       <b-col xl="2" cols="3" id="quickMenu" class="small">
         <h5>Column Title</h5>
         <div class="my-3">
-          <b-button variant="outline-secondary" size="sm" class="mr-2 my-1"> <b-icon icon="play-fill"></b-icon> Run </b-button>
-          <b-button variant="outline-secondary" size="sm" class="mr-2 my-1"> <b-icon icon="server"></b-icon> Save </b-button>
+          <b-button variant="outline-secondary" size="sm" class="mr-2 my-1" type="submit" form="code" > <b-icon icon="play-fill"></b-icon> Run </b-button>
+          <b-button variant="outline-secondary" size="sm" class="mr-2 my-1" type="submit" form="code" > <b-icon icon="server"></b-icon> Save </b-button>
         </div>
         <p>To do: Make this column a drawer or get rid of it.</p>
       </b-col>
       <b-col xl="5" cols="9" id="editor" class="mb-5">
         <b-card>
           <b-card-text>
-            <prism-editor class="my-editor" v-model="code" :highlight="highlighter" line-numbers />
+            <b-form @submit="onSubmit" id="code">
+              <prism-editor class="my-editor" v-model="form.code" :highlight="highlighter" line-numbers /> 
+            </b-form>
           </b-card-text>
         </b-card>
       </b-col>
       <b-col xl="5" id="output" class="small">
         <b-card>
           <b-card-text>
-            <span v-html="code"></span>
+            <span v-html="form.code"></span>
           </b-card-text>
         </b-card>
       </b-col>
@@ -50,27 +52,19 @@ export default {
     PrismEditor
   },
   data: () => ({ 
-    code: ` <!-- example -->
-<h1 class="display-4 mb-5">
-  The Main Languages of the Web
-</h1>
-
-<p class="lead mb-5">
-  HTML is the standard markup language for creating Web pages.
-</p>
-
-<p class="lead mb-5">
-  CSS is a language that describes how HTML elements are to be displayed.
-</p>
-
-<p class="lead mb-5">
-  JavaScript is the programming language of HTML and the Web.
-</p>
-`
+    form: {
+      code: `<p class="display-4">Welcome to Stream State <br> <span class="lead">Write code in Scala, Python, and HTML </span> </p>`,
+      created_by: '',
+      created: ''
+    }
   }),
   methods: {
     highlighter(code) {
-      return highlight(code, languages.js); //returns html
+      return highlight(code, languages.js);
+    },
+    onSubmit(event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
     }
   },
   props: ['user']
@@ -100,5 +94,8 @@ export default {
   } 
   .prism-editor__textarea:focus {
     outline: none;
+  }
+  .hidden {
+    visibility: hidden;
   }
 </style>
